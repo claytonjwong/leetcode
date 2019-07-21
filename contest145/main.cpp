dc2c7e295f60b762fcf2683e2902bfcbde6fe43b
Clayton Wong <clayton.wong@ookla.com>
2019-07-21 09:07:24 -0700

    contest 145: add solution for #3

--- contest145/main.cpp
+++ contest145/main.cpp
@@ -10,6 +10,9 @@
  * Q2: https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves/
  * A2: https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves/discuss/334579/C%2B%2B-Solution
  *
+ * Q3: https://leetcode.com/problems/longest-well-performing-interval/
+ * A3: https://leetcode.com/problems/longest-well-performing-interval/discuss/340389/C%2B%2B-solution
+ * 
  */
 
 
@@ -28,6 +31,7 @@
 
 using namespace std;
 
+/*
 class Solution {
 public:
     using VI = vector< int >;
@@ -52,7 +56,7 @@ int main() {
     copy( ans.begin(), ans.end(), ostream_iterator< int >( cout, " " ) );
     return 0;
 }
-
+*/
 
 /*
 struct TreeNode {
@@ -125,12 +129,96 @@ private:
         return ans;
     }
 };
+*/
+
+/*
+// naive TLE
+class Solution {
+public:
+    using VI = vector< int >;
+    int longestWPI( VI& A, int ans=0 ){
+        for( auto N = static_cast< int >( A.size() ), M{ N }; 0 <= M; --M ){
+            for( auto i{ 0 }; i + M <= N; ++i ){
+                auto easy{ 0 }, hard{ 0 };
+                for( auto j{ i }; j < i + M; ++j ){
+                    if( 8 < A[ j ] )
+                        ++hard;
+                    else
+                        ++easy;
+                }
+                if( easy < hard )
+                    return M;
+            }
+        }
+        return 0;
+    }
+};
+*/
+
+/*
+// verbose
+class Solution {
+public:
+    using VI = vector< int >;
+    using Map = unordered_map< int,int >;
+    int longestWPI( VI& A, Map M={}, int ans=0 ){
+        for( auto j{ 0 }, sum{ 0 }; j < A.size(); ++j ){
+            sum += ( A[ j ] > 8 )? 1 : -1;
+            if( 0 < sum ){
+                auto i = 0;
+                ans = j-i+1; // asbsolute, from index i == 0 inclusive to index j inclusive
+            }
+            if( M.find( sum ) == M.end() )
+                M[ sum ] = j; // store the index of the first occurrence of each unique sum
+            if( M.find( sum-1 ) != M.end() ){
+                auto i = M[ sum-1 ];
+                ans = max( ans, j-i ); // relative from index i == M[ sum-1 ] non-inclusive to index j inclusive ( j == M[ sum ] for each first occurrence of sum )
+            }
+        }
+        return ans;
+    }
+};
+*/
+
+// concise
+//class Solution {
+//public:
+//    using VI = vector< int >;
+//    using Map = unordered_map< int,int >;
+//    int longestWPI( VI& A, Map M={}, int ans=0 ){
+//        for( auto j{ 0 }, sum{ 0 }; j < A.size(); ++j ){
+//            sum += ( A[ j ] > 8 )? 1 : -1;
+//            if( 0 < sum )
+//                ans = j+1; // asbsolute, from index i == 0 inclusive to index j inclusive
+//            if( M.find( sum ) == M.end() )
+//                M[ sum ] = j; // store the index of the first occurrence of each unique sum
+//            if( M.find( sum-1 ) != M.end() )
+//                ans = max( ans, j - M[ sum-1 ] ); // relative from index i == M[ sum-1 ] non-inclusive to index j inclusive ( j == M[ sum ] for each first occurrence of sum )
+//        }
+//        return ans;
+//    }
+//};
+class Solution {
+public:
+    int longestWPI(vector<int> &hours) {
+        map<int, int> m = {{0, -1}}; //{non-tiring days - tiring days,  index}
+        int maxLen = 0, cnt = 0;
+        for (int i = 0; i < hours.size(); i++) {
+            cnt += hours[i] > 8 ? -1 : 1;
+            if( auto it = m.upper_bound(cnt); it != m.end() ) // cool!!!
+                maxLen = max(maxLen, i - it->second);
+            else
+                m.insert({cnt, i});
+        }
+        return maxLen;
+    }
+};
 
 int main() {
     Solution S;
-    Solution::VI A{ 6,9,9 };
+    vector<int> A{ 9,9,9 };
     auto ans = S.longestWPI( A );
     cout << ans << endl;
     return 0;
 }
-*/
+
