5c7ebe30e6911f040de5302b6c7cac0b9ab3cff5
Clayton Wong <clayton.wong@ookla.com>
2019-11-18 07:57:18 -0800

    1260. Shift 2D Grid

---
+++ 1260_shift_2d_grid/main.cpp
@@ -0,0 +1,49 @@
+/*
+ * 1260. Shift 2D Grid
+ *
+ * Q: https://leetcode.com/problems/shift-2d-grid/
+ * A: https://leetcode.com/problems/shift-2d-grid/discuss/432317/Javascript-and-C%2B%2B-solutions
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    using VI = vector<int>;
+    using VVI = vector<VI>;
+    VVI shiftGrid(VVI& A, int k, VI seq={}) {
+        int M = A.size(),
+            N = A[0].size();
+        k %= (M * N);
+        if (k == 0)
+            return A;
+        for (auto i=0; i < M; ++i)
+            for (auto j=0; j < N; ++j)
+                seq.push_back(A[i][j]);
+        auto pivot = seq.begin() + (M * N) - k;
+        VI shifted{pivot, seq.end()};
+        shifted.insert(shifted.end(), seq.begin(), pivot);
+        auto i = 0,
+             j = 0;
+        VVI ans(M, VI(N));
+        for (auto num: shifted) {
+            ans[i][j] = num;
+            if (++j == N)
+                ++i, j=0;
+        }
+        return ans;
+    }
+};
+
+int main() {
+    Solution solution;
+    Solution::VVI A{{1,2},{3,4}};
+    auto k = 1;
+    auto ans = solution.shiftGrid(A, k);
+    for (auto row: ans)
+        copy(row.begin(), row.end(), ostream_iterator<int>(cout, "")), cout << endl;
+    return 0;
+}
