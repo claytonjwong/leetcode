df522f0e844467cf5f7725c087d20929275fb4a4
Clayton Wong <clayton.wong@ookla.com>
2019-12-04 17:30:54 -0800

    1271. Hexspeak

---
+++ 1271_hexspeak/main.cpp
@@ -0,0 +1,36 @@
+/*
+ * 1271. Hexspeak
+ *
+ * Q: https://leetcode.com/problems/hexspeak/
+ * A: https://leetcode.com/problems/hexspeak/discuss/444208/Javascript-and-C%2B%2B-solutions
+ */
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string toHexspeak(string str, ostringstream os=ostringstream()) {
+        auto num = stoll(str);
+        os << hex << num;
+        auto A = os.str();
+        for (auto i=0; i < A.size(); ++i) {
+            if (A[i] == '0') A[i] = 'O';
+            if (A[i] == '1') A[i] = 'I';
+            if (!isalpha(A[i]))
+                return "ERROR";
+            A[i] = toupper(A[i]);
+        }
+        return A;
+    }
+};
+
+int main() {
+    Solution solution;
+    auto ans = solution.toHexspeak("747823223228"); // "AEIDBCDIBC"
+    cout << ans << endl;
+    return 0;
+}
+
