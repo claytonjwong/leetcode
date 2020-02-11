fd1ad46c83646bee08788d401f338e2051e73e28
Clayton Wong <claytonjwong@gmail.com>
2020-02-11 14:11:06 -0800

    40. Combination Sum II

---
+++ 40_combination_sum2/main.cpp
@@ -0,0 +1,53 @@
+/*
+ * 40. Combination Sum II
+ *
+ * Q: https://leetcode.com/problems/combination-sum-ii/
+ * A: https://leetcode.com/problems/combination-sum-ii/discuss/506360/Javascript-and-C%2B%2B-solutions
+ */
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    using VI = vector<int>;
+    using VVI = vector<VI>;
+    VVI combinationSum2(VI& A, int target, VVI ans = {}) {
+        dfs(A, target, ans);
+        return ans;
+    }
+private:
+    using Seen = unordered_set<string>;
+    void dfs(VI& A, int sum, VVI& ans, int start = 0, VI&& path = {}, Seen&& seen = {}) {
+        if (sum == 0) {
+            auto sorted = path;
+            sort(sorted.begin(), sorted.end());
+            ostringstream key;
+            copy(sorted.begin(), sorted.end(), ostream_iterator<int>(key, ","));
+            if (seen.insert(key.str()).second)
+                ans.push_back(sorted);
+            return;
+        }
+        for (auto i = start; i < A.size(); ++i) {
+            if (sum - A[i] < 0)
+                continue;
+            path.push_back(A[i]);
+            dfs(A, sum - A[i], ans, i + 1, move(path), move(seen));
+            path.pop_back();
+        }
+    }
+};
+
+int main() {
+    Solution solution;
+    Solution::VI A{2,5,2,1,2};
+    auto target = 5;
+    auto ans = solution.combinationSum2(A, target);
+    for (auto& row: ans)
+        copy(row.begin(), row.end(), ostream_iterator<int>(cout, " ")), cout << endl;
+    return 0;
+}
