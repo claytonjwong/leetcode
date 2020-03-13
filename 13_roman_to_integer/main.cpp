504e8e31dc57d5daa0c3f22053e4237191fc91ff
Clayton Wong <claytonjwong@gmail.com>
2020-03-13 15:58:35 -0700

    13. Roman to Integer (simplified)

--- 13_roman_to_integer/main.cpp
+++ 13_roman_to_integer/main.cpp
@@ -10,21 +10,36 @@
 
 using namespace std;
 
+//class Solution {
+//public:
+//    int romanToInt(string s, int val = 0, unordered_map<string, int> m = {{"I", 1}, {"IV", 4}, {"V", 5}, {"IX", 9}, {"X", 10}, {"XL", 40}, {"L", 50}, {"XC", 90}, {"C", 100}, {"CD", 400}, {"D", 500}, {"CM", 900}, {"M", 1000}}) {
+//        for (auto n = s.size(); s.size() > 1; n = s.size()) {
+//            auto two = s.substr(n - 2), one = s.substr(n - 1);
+//            if (m.find(two) != m.end())
+//                val += m[two], s = s.substr(0, n - 2);
+//            else
+//                val += m[one], s = s.substr(0, n - 1);
+//        }
+//        return val + (s.empty() ? 0 : m[s]);
+//    }
+//};
+
 class Solution {
 public:
-    int romanToInt(string s, int val = 0, unordered_map<string, int> m = {{"I", 1}, {"IV", 4}, {"V", 5}, {"IX", 9}, {"X", 10}, {"XL", 40}, {"L", 50}, {"XC", 90}, {"C", 100}, {"CD", 400}, {"D", 500}, {"CM", 900}, {"M", 1000}}) {
-        for (auto n = s.size(); s.size() > 1; n = s.size()) {
-            auto two = s.substr(n - 2), one = s.substr(n - 1);
-            if (m.find(two) != m.end())
-                val += m[two], s = s.substr(0, n - 2);
-            else
-                val += m[one], s = s.substr(0, n - 1);
+    using Map = unordered_map<char, int>;
+    int romanToInt(string s, int cur = 0, int pre = 0, int ans = 0) {
+        Map m{{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
+        for (auto c: string{s.rbegin(), s.rend()}) {
+            cur = m[c];
+            ans += cur < pre ? -cur : cur;
+            pre = m[c];
         }
-        return val + (s.empty() ? 0 : m[s]);
+        return ans;
     }
 };
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Solution solution;
+    cout << solution.romanToInt("III") << endl;
     return 0;
 }
