4d5576fa9866751f86810e539a05ec75a469d489
Clayton Wong <claytonjwong@gmail.com>
2020-04-23 08:47:39 -0700

    594. Longest Harmonious Subsequence

---
+++ 594_longest_harmonious_subsequence/main.cpp
@@ -0,0 +1,31 @@
+/*
+ * 594. Longest Harmonious Subsequence
+ *
+ * Q: https://leetcode.com/problems/longest-harmonious-subsequence/
+ * A: https://leetcode.com/problems/longest-harmonious-subsequence/discuss/592722/Javascript-and-C%2B%2B-solutions
+ */
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    using VI = vector<int>;
+    using Map = unordered_map<int, int>;
+    int findLHS(VI& A, Map m = {}, int max = 0) {
+        for (auto x: A)
+            ++m[x];
+        for (auto& [x, _]: m)
+            if (m.find(x - 1) != m.end())
+                max = std::max(max, m[x - 1] + m[x]);
+        return max;
+    }
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
