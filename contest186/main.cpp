0b2bd8b6083c430f1c389aa8c131940f3ae0b455
Clayton Wong <claytonjwong@gmail.com>
2020-04-26 11:32:13 -0700

    contest186 (4671 / 11684)

--- contest186/main.cpp
+++ contest186/main.cpp
@@ -59,7 +59,7 @@ namespace Question1_Prefix_Sums {
  * Q: https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/
  * A: https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/discuss/597883/Javascript-and-C%2B%2B-solutions
  */
-namespace Question2 {
+namespace Question2_ForLoop {
     class Solution {
     public:
         using VI = vector<int>;
@@ -67,7 +67,22 @@ namespace Question2 {
             int N = A.size();
             auto sum = accumulate(A.begin() + N - K, A.end(), 0),
                  max = sum;
-            for (auto i{ 0 }, j{ N - K }; K--; max = std::max(max, sum += A[i++] - A[j++]));
+            for (auto i{ 0 }, j{ N - K }; K--; max = std::max(max, sum += A[i++] - A[j++])); // slide window by K 👉
+            return max;
+        }
+    };
+}
+namespace Question2_WhileLoop {
+    class Solution {
+    public:
+        using VI = vector<int>;
+        int maxScore(VI& A, int K) {
+            int N = A.size(),
+                i = 0,
+                j = N - K;
+            auto sum = accumulate(A.begin() + j, A.end(), 0),
+                 max = sum;
+            while (K--) max = std::max(max, sum += A[i++] - A[j++]); // slide window by K 👉
             return max;
         }
     };
