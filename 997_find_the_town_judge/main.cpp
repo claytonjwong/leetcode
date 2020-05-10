acf2c9c0a2d17b4d4d25121553e8fb1c4194183a
Clayton Wong <claytonjwong@gmail.com>
2020-05-10 07:32:21 -0700

    997. Find the Town Judge (alt solution)

--- 997_find_the_town_judge/main.cpp
+++ 997_find_the_town_judge/main.cpp
@@ -12,24 +12,47 @@
 
 using namespace std;
 
-class Solution {
-public:
-    using VI = vector<int>;
-    using VVI = vector<VI>;
-    using Trust = unordered_set<int>;
-    using Trusts = unordered_map<int, Trust>;
-    int findJudge(int N, VVI& A, Trusts T = {}, Trust all = {}) {
-        for (auto& t: A) {
-            auto [a, b] = tie(t[0], t[1]);
-            all.insert(a); // all whom have trust
-            T[b].insert(a); // b is trusted by a
+namespace Solution_Set {
+    class Solution {
+    public:
+        using VI = vector<int>;
+        using VVI = vector<VI>;
+        using Trust = unordered_set<int>;
+        using Trusts = unordered_map<int, Trust>;
+        int findJudge(int N, VVI& A, Trusts T = {}, Trust all = {}) {
+            for (auto& t: A) {
+                auto [a, b] = tie(t[0], t[1]);
+                all.insert(a); // all whom have trust
+                T[b].insert(a); // b is trusted by a
+            }
+            for (auto [x, t]: T)
+                if (all.find(x) == all.end() && t.size() == N - 1) // x trusts no one and is trusted by N - 1 others 🎯
+                    return x;
+            return N == 1 ? 1 : -1;
         }
-        for (auto [x, t]: T)
-            if (all.find(x) == all.end() && t.size() == N - 1) // x trusts no one and is trusted by N - 1 others 🎯
-                return x;
-        return N == 1 ? 1 : -1;
-    }
-};
+    };
+}
+
+namespace Solution_Array {
+    class Solution {
+    public:
+        using VI = vector<int>;
+        using VVI = vector<VI>;
+        int findJudge(int N, VVI& T) {
+            VI A(N + 1), // +1 for 1-based indexing
+                    B(N + 1);
+            for (auto& t: T) {
+                auto [a, b] = tie(t[0], t[1]);
+                ++A[a];
+                ++B[b];
+            }
+            for (auto i{ 1 }; i <= N; ++i)
+                if (!A[i] && B[i] == N - 1)
+                    return i;
+            return -1;
+        }
+    };
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
