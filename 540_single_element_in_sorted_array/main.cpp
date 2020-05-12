1c58f887094b043718a11359eee5f54d81c305f0
Clayton Wong <claytonjwong@gmail.com>
2020-05-12 06:10:46 -0700

    540. Single Element in a Sorted Array (xor solution + edits for readability)

--- 540_single_element_in_sorted_array/main.cpp
+++ 540_single_element_in_sorted_array/main.cpp
@@ -10,29 +10,42 @@
 
 using namespace std;
 
-class Solution {
-public:
-    using VI = vector<int>;
-    int singleNonDuplicate(VI& A) {
-        int N = A.size(),
-            i = 0,
-            j = N;
-        for (;;) {
-            auto k = (i + j) / 2;
-            auto min = k,
-                 max = k;
-            if (k > 0 && A[k - 1] == A[k])
-                min = k - 1;
-            if (k < N - 1 && A[k] == A[k + 1])
-                max = k + 1;
-            if (min == k && max == k) // target 🎯
-                return A[k];
-            if (min % 2) j = min; // 👈 left half has odd length
-            if (max % 2) i = max; // 👉 right half has odd length
+namespace XOR {
+    class Solution {
+    public:
+        using VI = vector<int>;
+        int singleNonDuplicate(VI& A, int XOR = 0) {
+            for_each(A.begin(), A.end(), [&](auto x) { XOR ^= x; });
+            return XOR;
         }
-        return 1e9 + 7;
-    }
-};
+    };
+}
+
+namespace BinarySearch {
+    class Solution {
+    public:
+        using VI = vector<int>;
+        int singleNonDuplicate(VI& A) {
+            int N = A.size(),
+                i = 0,
+                j = N,
+                beg = 0,
+                end = N - 1;
+            for (;;) {
+                auto k = (i + j) / 2;
+                auto min = k,
+                     max = k;
+                if (k > beg && A[k] == A[k - 1]) min = k - 1;
+                if (k < end && A[k] == A[k + 1]) max = k + 1;
+                if (min == k && max == k) // target 🎯
+                    return A[k];
+                if (min % 2) j = min; // 👈 left half has odd length
+                if (max % 2) i = max; // 👉 right half has odd length
+            }
+        }
+    };
+}
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
