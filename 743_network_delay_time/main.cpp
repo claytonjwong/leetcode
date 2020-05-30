6330c4edb8d8ccfb4ac3f70d18ffa32199502fa7
Clayton Wong <claytonjwong@gmail.com>
2020-05-30 06:27:42 -0700

    743. Network Delay Time (add SPFA + Dijkstra)

--- 743_network_delay_time/main.cpp
+++ 743_network_delay_time/main.cpp
@@ -7,27 +7,93 @@
 
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
-class Solution {
-public:
-    using VI = vector<int>;
-    using VVI = vector<VI>;
-    int networkDelayTime(VVI& E, int N, int K, int INF = 1e9 + 7) {
-        VI dist(N + 1, INF); // +1 for 1-based indexing
-        dist[K] = 0;
-        while (--N) {
+namespace BellmanFord {
+    class Solution {
+    public:
+        using VI = vector<int>;
+        using VVI = vector<VI>;
+        int networkDelayTime(VVI& E, int N, int K, int INF = 1e9 + 7) {
+            VI dist(N + 1, INF);   // +1 for 1-based indexing
+            dist[0] = dist[K] = 0; // dist = 0 for sentinel at index 0 and node K
+            while (--N) {
+                for (auto& edge: E) {
+                    auto [u, v, w] = tie(edge[0], edge[1], edge[2]);
+                    if (dist[v] > dist[u] + w)
+                        dist[v] = dist[u] + w;
+                }
+            }
+            auto max = *max_element(dist.begin(), dist.end());
+            return max < INF ? max : -1;
+        }
+    };
+}
+
+namespace SPFA {
+    class Solution {
+    public:
+        using VI = vector<int>;
+        using VVI = vector<VI>;
+        using Graph = vector<vector<pair<int, int>>>;
+        int networkDelayTime(VVI& E, int N, int K, int INF = 1e9 + 7) {
+            Graph adj(N + 1); // +1 for 1-based indexing
             for (auto& edge: E) {
                 auto [u, v, w] = tie(edge[0], edge[1], edge[2]);
-                if (dist[v] > dist[u] + w)
-                    dist[v] = dist[u] + w;
+                adj[u].push_back({ v, w });
             }
+            VI dist(N + 1, INF);   // +1 for 1-based indexing
+            dist[0] = dist[K] = 0; // set init dist = 0 for sentinel at index 0 and for node K
+            queue<int> q{{ K }};
+            while (!q.empty()) {
+                auto u = q.front(); q.pop();
+                for (auto [v, w]: adj[u]) {
+                    if (dist[v] > dist[u] + w) {
+                        dist[v] = dist[u] + w;
+                        q.push(v);
+                    }
+                }
+            }
+            auto max = *max_element(dist.begin(), dist.end());
+            return max < INF ? max : -1;
         }
-        auto max = *max_element(dist.begin() + 1, dist.end()); // +1 to exclude sentinel at index 0
-        return max < INF ? max : -1;
-    }
-};
+    };
+}
+
+namespace Dijkstra {
+    class Solution {
+    public:
+        using VI = vector<int>;
+        using VVI = vector<VI>;
+        using Pair = pair<int, int>;
+        using Pairs = vector<Pair>;
+        using Graph = vector<Pairs>;
+        int networkDelayTime(VVI& E, int N, int K, int INF = 1e9 + 7) {
+            Graph adj(N + 1); // +1 for 1-based indexing
+            for (auto& edge: E) {
+                auto [u, v, w] = tie(edge[0], edge[1], edge[2]);
+                adj[u].push_back({ v, w });
+            }
+            VI dist(N + 1, INF);   // +1 for 1-based indexing
+            dist[0] = dist[K] = 0; // set init dist = 0 for sentinel at index 0 and for node K
+            priority_queue<Pair, Pairs, greater<Pair>> q;
+            q.push({ K, 0 });
+            while (!q.empty()) {
+                auto [u, _] = q.top(); q.pop();
+                for (auto [v, w]: adj[u]) {
+                    if (dist[v] > dist[u] + w) {
+                        dist[v] = dist[u] + w;
+                        q.push({ v, dist[v] });
+                    }
+                }
+            }
+            auto max = *max_element(dist.begin(), dist.end());
+            return max < INF ? max : -1;
+        }
+    };
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
