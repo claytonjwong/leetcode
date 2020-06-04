9664afacb9443acf472120b313e63d08bcb19b02
Clayton Wong <claytonjwong@gmail.com>
2020-06-04 05:32:58 -0700

    509. Fibonacci Number

---
+++ 509_fibonacci_number/main.cpp
@@ -0,0 +1,36 @@
+/*
+ * 509. Fibonacci Number
+ *
+ * Q: https://leetcode.com/problems/distribute-candies/
+ * A: https://leetcode.com/problems/distribute-candies/discuss/668969/Javascript-and-C%2B%2B-solutions
+ */
+
+#include <iostream>
+
+using namespace std;
+
+namespace TopDown {
+    class Solution {
+    public:
+        int fib(int N) {
+            return N < 2 ? N : fib(N - 1) + fib(N - 2);
+        }
+    };
+}
+
+namespace BottomUp {
+    class Solution {
+    public:
+        int fib(int N, int a = 0, int b = 1, int c = 1) {
+            if (N < 2)
+                return N;
+            for (; --N; c = a + b, a = b, b = c);
+            return c;
+        }
+    };
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
