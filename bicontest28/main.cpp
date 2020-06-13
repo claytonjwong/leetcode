14ac84699211582e0f898004e39ce57e8a3a7a23
Clayton Wong <claytonjwong@gmail.com>
2020-06-13 11:50:12 -0700

    bicontest28 (1858 / 6427)

--- bicontest28/main.cpp
+++ bicontest28/main.cpp
@@ -16,26 +16,43 @@ using namespace std;
  * Q: https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/
  * A: https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/discuss/685454/Javascript-and-C%2B%2B-solutions
  */
-namespace Question1 {
+namespace Question1_BruteForce {
     class Solution {
     public:
         using VI = vector<int>;
         VI finalPrices(VI& A) {
             int N = A.size();
-            VI ans{ A };
             for (auto i{ 0 }; i < N; ++i) {
                 for (auto j{ i + 1 }; j < N; ++j) {
-                    if (A[j] <= A[i]) {
-                        ans[i] -= A[j];
+                    if (A[i] >= A[j]) {
+                        A[i] -= A[j]; // apply discount 🎯
                         break;
                     }
                 }
             }
-            return ans;
+            return A;
         }
     };
 }
-
+namespace Question1_Stack {
+    class Solution {
+    public:
+        using VI = vector<int>;
+        VI finalPrices(VI& A, VI S = {}) {
+            for (auto j{ 0 }; j < A.size(); ++j) {
+                while (!S.empty()) {
+                    auto i = S.back(); // i is the top of the stack
+                    if (A[i] < A[j])
+                        break;
+                    A[i] -= A[j]; // A[i] >= A[j] apply discount 🎯
+                    S.pop_back();
+                }
+                S.push_back(j); // push current j to stack as previous i
+            }
+            return A;
+        }
+    };
+}
 
 /*
  * 1476. Subrectangle Queries
