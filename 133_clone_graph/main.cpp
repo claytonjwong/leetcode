a0bb40f47233f68d84e6ea99c64cb6f14a0e036d
Clayton Wong <claytonjwong@gmail.com>
2020-06-26 17:14:15 -0700

    133. Clone Graph (add concise solution)

--- 133_clone_graph/main.cpp
+++ 133_clone_graph/main.cpp
@@ -17,23 +17,42 @@ struct Node {
     Node (int val) : val{val} {}
 };
 
-class Solution {
-public:
-    Node* cloneGraph(Node* node) {
-        return node ? go(node) : nullptr;
-    }
-private:
-    using Map = unordered_map<int, Node*>;
-    Node* go(Node* node, Map&& seen = {}) {
-        if (seen.find(node->val) != seen.end())
-            return seen[node->val];
-        auto copy = new Node(node->val);
-        seen[node->val] = copy;
-        for (auto nei: node->neighbors)
-            copy->neighbors.push_back(go(nei, move(seen)));
-        return copy;
-    }
-};
+namespace Concise {
+    class Solution {
+    public:
+        using Map = unordered_map<Node*, Node*>;
+        Node* cloneGraph(Node* node, Map&& m = {}) {
+            if (!node)
+                return nullptr;
+            if (m.find(node) != m.end())
+                return m[node];
+            m[node] = new Node(node->val);
+            for (auto nei: node->neighbors)
+                m[node]->neighbors.push_back(cloneGraph(nei, move(m)));
+            return m[node];
+        }
+    };
+}
+
+namespace Verbose {
+    class Solution {
+    public:
+        Node* cloneGraph(Node* node) {
+            return node ? go(node) : nullptr;
+        }
+    private:
+        using Map = unordered_map<int, Node*>;
+        Node* go(Node* node, Map&& seen = {}) {
+            if (seen.find(node->val) != seen.end())
+                return seen[node->val];
+            auto copy = new Node(node->val);
+            seen[node->val] = copy;
+            for (auto nei: node->neighbors)
+                copy->neighbors.push_back(go(nei, move(seen)));
+            return copy;
+        }
+    };
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
