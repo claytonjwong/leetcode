2c97cb52d1adc5793709015abbaf1dc9208cfa22
Clayton Wong <claytonjwong@gmail.com>
2020-06-30 08:29:46 -0700

    746. Min Cost Climbing Stairs (update comment for readability)

--- 746_min_cost_climbing_stairs/main.cpp
+++ 746_min_cost_climbing_stairs/main.cpp
@@ -19,13 +19,13 @@ namespace TopDownBruteForce {
         int go(int i) {
             if (N <= i)
                 return 0; // 🛑  base case: we reached the top floor
-            return A[i] + min(go(i + 1), go(i + 2)); // cost of i-th stair plus min of (i + 1)-th and (i + 2)-th solution 🎯
+            return A[i] + min(go(i + 1), go(i + 2)); // cost of i-th stair plus min(one step or two steps) 🎯
         }
     public:
         int minCostClimbingStairs(VI& A_) {
             A = A_;
             N = A.size();
-            return min(go(0), go(1));
+            return min(go(0), go(1)); // start on min(first step, second step)
         }
     };
 }
@@ -41,13 +41,13 @@ namespace TopDownMemo {
                 return m[i]; // memo 🤔
             if (N <= i)
                 return m[i] = 0; // 🛑  base case: we reached the top floor
-            return m[i] = A[i] + min(go(i + 1, move(m)), go(i + 2, move(m))); // cost of i-th stair plus min of (i + 1)-th and (i + 2)-th solution 🎯
+            return m[i] = A[i] + min(go(i + 1, move(m)), go(i + 2, move(m))); // cost of i-th stair plus min(one step or two steps) 🎯
         }
     public:
         int minCostClimbingStairs(VI& A_) {
             A = A_;
             N = A.size();
-            return min(go(0), go(1));
+            return min(go(0), go(1)); // start on min(first step, second step)
         }
     };
 }
@@ -63,7 +63,7 @@ namespace BottomUp {
             dp[0] = A[0]; // 🛑  base case: start on first step
             dp[1] = A[1]; // 🛑  base case: start on second step
             for (auto i{2}; i < N; ++i)
-                dp[i] = A[i] + min(dp[i - 2], dp[i - 1]); // cost of i-th stair plus min of (i - 2)-th and (i - 1)-th solution 🎯
+                dp[i] = A[i] + min(dp[i - 2], dp[i - 1]); // cost of i-th stair plus min(one step or two steps) 🎯
             return min(dp[N - 2], dp[N - 1]); // N-th stair is reached from min of one or two stairs away
         }
     };
@@ -79,7 +79,7 @@ namespace BottomUpMemOpt {
                  b = A[1], // 🛑  base case: start on second step
                  c = -1;
             for (auto i{ 2 }; i < N; ++i, a = b, b = c)
-                c = A[i] + min(a, b); // cost of i-th stair plus min of (i - 2)-th and (i - 1)-th solution 🎯
+                c = A[i] + min(a, b); // cost of i-th stair plus min(one step or two steps) 🎯
             return min(a, b); // N-th stair is reached from min of one or two stairs away
         }
     };
