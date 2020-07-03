53873b3184b8922566b40dac5696d6c46e7c6a34
Clayton Wong <claytonjwong@gmail.com>
2020-07-03 08:42:42 -0700

    957. Prison Cells After N Days (edits for conciseness)

--- 957_prison_cells_after_N_days/main.cpp
+++ 957_prison_cells_after_N_days/main.cpp
@@ -18,10 +18,9 @@ public:
     using VVI = vector<VI>;
     using Map = unordered_map<string, int>;
     VI prisonAfterNDays(VI& A, int N, VVI all = {}, Map m = {}, int P = 0) {
-        VI cur{ A },
-           pre{ A };
+        VI pre{ A };
         for (auto day{ 0 }; day <= N; ++day) {
-            all.push_back(pre); // 👀  track all cells seen
+            all.push_back(pre); // 👀 track all seen
             // ⭐️ find pisano period to avoid TLE for large values of N
             ostringstream key; copy(pre.begin(), pre.end(), ostream_iterator<int>(key, ""));
             if (m.find(key.str()) != m.end()) {
@@ -30,14 +29,13 @@ public:
             }
             m[key.str()] = day;
             // 🤔 generate current from previous
-            cur[0] = cur[7] = 0; // first and last are always set to 0
+            VI cur(8);
             for (auto i{ 1 }; i < 7; ++i)
                 cur[i] = int(pre[i - 1] == pre[i + 1]);
             swap(pre, cur);
         }
-        return pre == cur ? cur             // case 1: if previous equals current, then pattern is immutable until N-th day
-             : !P ? all.back()              // case 2: pisano period not reached in N days, return N-th day's solution
-             : N % P ? all[N % P] : all[P]; // case 3: return N-th day's solution based upon the pisano period
+        return !P ? all[all.size() - 1]     // ❌ case 1: pisano period *not* found in N days, return N-th day's solution
+             : N % P ? all[N % P] : all[P]; // ✅ case 2: pisano period found, return N-th day's solution based upon pisano period
     }
 };
 
