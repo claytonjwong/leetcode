b8cdf9ed62cf3134c088a2b44412d02b98986faf
Clayton Wong <claytonjwong@gmail.com>
2020-07-14 08:11:36 -0700

    238. Product of Array Except Self

--- 238_product_array_except_self/main.cpp
+++ 238_product_array_except_self/main.cpp
@@ -13,17 +13,22 @@ using namespace std;
 class Solution {
 public:
     using VI = vector<int>;
-    VI productExceptSelf(VI& A, VI ans = {}) {
+    VI productExceptSelf(VI& A) {
         int N = A.size();
-        VI L(N + 1, 1),
-           R(N + 1, 1);
-        for (auto i = 1; i <= N; ++i)
-            L[i] = L[i - 1] * A[i - 1];
-        for (auto i = N - 1; i >= 0; --i)
-            R[i] = R[i + 1] * A[i];
-        for (auto i = 0; i < N; ++i)
-            ans.push_back(L[i] * R[i + 1]);
-        return ans;
+        VI L{ A },
+                R{ A };
+        auto beg = 0,
+                end = N - 1;
+        for (auto i{ beg + 1 }; i <  N; ++i) L[i] = L[i - 1] * A[i]; // ⭐️ prefix products from 👉 (L)eft-to-right
+        for (auto j{ end - 1 }; 0 <= j; --j) R[j] = R[j + 1] * A[j]; // ⭐️ prefix products from 👈 (R)ight-to-left
+        transform(A.begin(), A.end(), A.begin(), [i = -1, beg, end, &L, &R](auto _) mutable {
+            ++i;
+            auto prod{ 1 };
+            if (beg < i) prod *= L[i - 1];
+            if (i < end) prod *= R[i + 1];
+            return prod;
+        });
+        return A;
     }
 };
 
