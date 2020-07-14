f6663985063a7ab1a82412f134b71d4eee0ca70b
Clayton Wong <claytonjwong@gmail.com>
2020-07-14 08:11:09 -0700

    1344. Angle Between Hands of a Clock (add verbose solution)

--- 1344_angle_between_hands_of_clock/main.cpp
+++ 1344_angle_between_hands_of_clock/main.cpp
@@ -11,13 +11,29 @@
 
 using namespace std;
 
-class Solution {
-public:
-    double angleClock(int h, int m) {
-        auto ans = abs((h % 12 * 30 + m / 2.0) - (m * 6));
-        return min(ans, 360 - ans);
-    }
-};
+namespace Concise {
+    class Solution {
+    public:
+        double angleClock(int h, int m) {
+            auto ans = abs((h % 12 * 30 + m / 2.0) - (m * 6));
+            return min(ans, 360 - ans);
+        }
+    };
+}
+
+namespace Verbose {
+    class Solution {
+    public:
+        double angleClock(int h, int m, double offset = 1e5) {
+            if (h == 12)
+                h = 0;
+            h = offset * (30 * h + 0.5 * m);
+            m = offset * (6 * m);
+            auto degree = max(h, m) - min(h, m);
+            return min(degree / offset, 360 - degree / offset);
+        }
+    };
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
