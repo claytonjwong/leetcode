d06b8b4fc7e33eb0e635ed382a919106394d58fc
Clayton Wong <claytonjwong@gmail.com>
2020-07-19 07:57:27 -0700

    67. Add Binary (more concise code)

--- 67_add_binary/main.cpp
+++ 67_add_binary/main.cpp
@@ -2,7 +2,7 @@
  * 67. Add Binary
  *
  * Q: https://leetcode.com/problems/add-binary/
- * A: https://leetcode.com/problems/add-binary/discuss/606766/Javascript-and-C%2B%2B-solutions
+ * A: https://leetcode.com/problems/add-binary/discuss/744220/Javascript-Python3-C%2B%2B-accumulate-sum-a%2Bb%2Bc-in-reverse
  */
 
 #include <iostream>
@@ -11,19 +11,16 @@ using namespace std;
 
 class Solution {
 public:
-    string addBinary(string A, string B, int carry = 0, string ans = {}) {
+    string addBinary(string A, string B, int c = 0, string ans = {}) { // ⭐️ c is the carry
         int M = A.size(),
             N = B.size();
-        reverse(A.begin(), A.end());
-        reverse(B.begin(), B.end());
-        for (auto i{ 0 }; i < max(A.size(), B.size()); ++i) {
-            auto x = (i < M && A[i] == '1')
-                   + (i < N && B[i] == '1')
-                   + carry;
-            ans.push_back((x & 1) + '0');
-            carry = x > 1;
+        for (auto i{ M - 1 }, j{ N - 1 }; 0 <= i || 0 <= j; --i, --j) {
+            auto a = 0 <= i ? A[i] - '0' : 0,
+                 b = 0 <= j ? B[j] - '0' : 0;
+            ans.push_back((a + b + c) % 2 + '0');
+            c = 1 < a + b + c;
         }
-        if (carry)
+        if (c)
             ans.push_back('1');
         return { ans.rbegin(), ans.rend() };
     }
