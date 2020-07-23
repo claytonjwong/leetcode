8f0cafcd3e036b25ba0de46ca73b24707af8dfba
Clayton Wong <claytonjwong@gmail.com>
2020-07-23 08:00:49 -0700

    1376. Time Needed to Inform All Employees (DFS + BFS)

--- 1376_time_to_inform_all_employees/main.cpp
+++ 1376_time_to_inform_all_employees/main.cpp
@@ -8,26 +8,55 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <queue>
 
 using namespace std;
 
-//class Solution {
-//public:
-//    using VI = vector<int>;
-//    using Map = unordered_map<int, VI>;
-//    int numOfMinutes(int N, int root, VI& parent, VI& cost, Map adj = {}) {
-//        for (auto i = 0; i < N; ++i)
-//            if (parent[i] > -1)
-//                adj[parent[i]].push_back(i);
-//        return go(root, cost, adj);
-//    }
-//private:
-//    int go(int i, VI& cost, Map& adj, int max = 0) {
-//        for (auto j: adj[i])
-//            max = std::max(max, cost[i] + go(j, cost, adj));
-//        return max;
-//    }
-//};
+namespace DFS {
+    class Solution {
+        using VI = vector<int>;
+        using Map = unordered_map<int, VI>;
+        VI cost;
+        Map adj;
+        int go(int i, int max = 0) {
+            for (auto j: adj[i])
+                max = std::max(max, go(j));
+            return cost[i] + max;
+        }
+    public:
+        int numOfMinutes(int N, int root, VI& A, VI& cost_) {
+            cost = cost_;
+            for (auto i{ 0 }; i < N; ++i)
+                if (-1 < A[i])
+                    adj[A[i]].push_back(i);
+            return go(root);
+        }
+    };
+}
+
+namespace BFS {
+    class Solution {
+    public:
+        using VI = vector<int>;
+        using Map = unordered_map<int, VI>;
+        int numOfMinutes(int N, int root, VI& A, VI& cost, Map adj = {}, int max = 0) {
+            for (auto i = 0; i < N; ++i)
+                if (-1 < A[i])
+                    adj[A[i]].push_back(i);
+            VI dist(N);
+            queue<int> q{{ root }};
+            while (q.size()) {
+                auto i = q.front(); q.pop();
+                for (auto j: adj[i]) {
+                    dist[j] = dist[i] + cost[i];
+                    max = std::max(max, dist[j]);
+                    q.push(j);
+                }
+            }
+            return max;
+        }
+    };
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
