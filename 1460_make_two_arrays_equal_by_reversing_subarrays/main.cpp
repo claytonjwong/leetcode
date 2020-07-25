8cac246d0232ed31de054b508b057133d60b4bbc
Clayton Wong <claytonjwong@gmail.com>
2020-07-25 10:17:29 -0700

    1460. Make Two Arrays Equal by Reversing Sub-arrays

--- 1460_make_two_arrays_equal_by_reversing_subarrays/main.cpp
+++ 1460_make_two_arrays_equal_by_reversing_subarrays/main.cpp
@@ -11,16 +11,30 @@
 
 using namespace std;
 
-class Solution {
-public:
-    using VI = vector<int>;
-    using Map = unordered_map<int, int>;
-    bool canBeEqual(VI& T, VI& A, Map m = {}) {
-        for (auto x: T) ++m[x];
-        for (auto x: A) --m[x];
-        return all_of(m.begin(), m.end(), [](auto& pair) { return !pair.second; });
-    }
-};
+namespace CountFrequency {
+    class Solution {
+    public:
+        using VI = vector<int>;
+        using Map = unordered_map<int, int>;
+        bool canBeEqual(VI& T, VI& A, Map m = {}) {
+            for (auto x: T) ++m[x];
+            for (auto x: A) --m[x];
+            return all_of(m.begin(), m.end(), [](auto& pair) { return !pair.second; });
+        }
+    };
+}
+
+namespace SortAndCompare {
+    class Solution {
+    public:
+        using VI = vector<int>;
+        bool canBeEqual(VI& A, VI& B) {
+            sort(A.begin(), A.end());
+            sort(B.begin(), B.end());
+            return A == B;
+        }
+    };
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
