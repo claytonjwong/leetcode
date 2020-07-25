b22861ef432649fce1a75bb2fecaab063d2d8033
Clayton Wong <claytonjwong@gmail.com>
2020-07-25 14:32:20 -0700

    283. Move Zeroes

--- 283_move_zeros/main.cpp
+++ 283_move_zeros/main.cpp
@@ -2,7 +2,7 @@
  * 283. Move Zeroes
  *
  * Q: https://leetcode.com/problems/move-zeroes/
- * A: https://leetcode.com/problems/move-zeroes/discuss/563206/Javascript-and-C%2B%2B-solutions
+ * A: https://leetcode.com/problems/move-zeroes/discuss/563206/Javascript-Python3-C%2B%2B-readwrite-index
  */
 
 #include <iostream>
@@ -10,17 +10,38 @@
 
 using namespace std;
 
-class Solution {
-public:
-    using VI = vector<int>;
-    void moveZeroes(VI& A) {
-        int N = A.size();
-        for (auto i{ 0 }, j{ 0 }; i < N; ++i) {
-            if (A[i] == 0 && A[j] != 0) j = i; // j-th write index with value 0
-            if (A[j] == 0 && A[i] != 0) swap(A[i], A[j++]); // set i-th value to j-th write index
+namespace Simple {
+    class Solution {
+    public:
+        using VI = vector<int>;
+        void moveZeroes(VI& A) {
+            int N = A.size(),
+                i{ 0 },
+                j{ 0 };
+            while (j < N) {
+                if (A[j])
+                    A[i++] = A[j]; // ✅ write each non-zero j-th value, ie. 🚫 skip each zero j-th value 🎱
+                ++j;
+            }
+            while (i < N)
+                A[i++] = 0; // write each 🚫 skipped zero j-th value 🎱 at the end
         }
-    }
-};
+    };
+}
+
+namespace Complex {
+    class Solution {
+    public:
+        using VI = vector<int>;
+        void moveZeroes(VI& A) {
+            int N = A.size();
+            for (auto i{ 0 }, j{ 0 }; i < N; ++i) {
+                if (A[i] == 0 && A[j] != 0) j = i; // j-th write index with value 0
+                if (A[j] == 0 && A[i] != 0) swap(A[i], A[j++]); // set i-th value to j-th write index
+            }
+        }
+    };
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
