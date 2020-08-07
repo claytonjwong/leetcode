638225a325cf759922af28efe479dcac687f1198
Clayton Wong <claytonjwong@gmail.com>
2020-08-07 09:30:47 -0700

    987. Vertical Order Traversal of a Binary Tree

---
+++ 987_vertical_order_traversal/main.cpp
@@ -0,0 +1,52 @@
+/*
+ * 987. Vertical Order Traversal of a Binary Tree
+ *
+ * Q: https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/
+ * A: https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/discuss/778164/Javascript-and-C%2B%2B-solutions
+ */
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <map>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left, *right;
+};
+
+class Solution {
+public:
+    using VI = vector<int>;
+    using VVI = vector<VI>;
+    using Map = unordered_map<int, map<int, VI>>;
+    using fun = function<void(TreeNode*, int, int)>;
+    VVI verticalTraversal(TreeNode* root, Map m = {}, VVI ans = {}) {
+        fun go = [&](TreeNode* root, int x = 0, int y = 0) {
+            if (!root)
+                return;
+            m[x][y].push_back(root->val);
+            go(root->left, x - 1, y + 1);
+            go(root->right, x + 1, y + 1);
+        };
+        go(root, 0, 0);
+        for (auto x{ -999 }; x <= 999; ++x) {
+            if (m.find(x) == m.end())
+                continue;
+            for (auto& [_, nodes]: m[x])
+                sort(nodes.begin(), nodes.end()); // sort nodes with same y by value
+            VI cur;
+            for (auto& [_, nodes]: m[x])
+                cur.insert(cur.end(), nodes.begin(), nodes.end());
+            ans.push_back(cur);
+        }
+        return ans;
+    }
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
