a2bb6d20cc030c0a8f1bbb0d22f1ce7f223bd1da
Clayton Wong <claytonjwong@gmail.com>
2020-08-10 07:04:05 -0700

    171. Excel Sheet Column Number

--- 171_excel_sheet_column_number/main.cpp
+++ 171_excel_sheet_column_number/main.cpp
@@ -2,23 +2,35 @@
  * 171. Excel Sheet Column Number
  *
  * Q: https://leetcode.com/problems/excel-sheet-column-number/
- * A: https://leetcode.com/problems/excel-sheet-column-number/discuss/594372/Javascript-and-C%2B%2B-solutions
+ * A: https://leetcode.com/problems/excel-sheet-column-number/discuss/594372/Javascript-Python3-C%2B%2B-1-Liners
  */
 
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-class Solution {
-public:
-    using LL = long long;
-    int titleToNumber(string S, int ans = 0) {
-        int N = S.size();
-        for (LL i{ N - 1 }, j{ 1 }; i >= 0; --i, j *= 26)
-            ans += (S[i] - 'A' + 1) * j;
-        return ans;
-    }
-};
+namespace concise {
+    class Solution {
+    public:
+        int titleToNumber(string s, int i = 0) {
+            return i == s.size() ? 0 : (s[s.size() - (i + 1)] - 64) * pow(26, i) + titleToNumber(s, i + 1);
+        }
+    };
+}
+
+namespace verbose {
+    class Solution {
+    public:
+        using LL = long long;
+        int titleToNumber(string S, int ans = 0) {
+            int N = S.size();
+            for (LL i{ N - 1 }, j{ 1 }; i >= 0; --i, j *= 26)
+                ans += (S[i] - 'A' + 1) * j;
+            return ans;
+        }
+    };
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
