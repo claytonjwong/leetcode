410b992af79760cc47f651d3f8d7cc4a8f1ddc91
Clayton Wong <claytonjwong@gmail.com>
2020-10-03 14:38:39 -0700

    1602. Find Nearest Right Node in Binary Tree (DFS)

--- 1602_nearest_right_node/main.cpp
+++ 1602_nearest_right_node/main.cpp
@@ -2,7 +2,7 @@
  * 1602. Find Nearest Right Node in Binary Tree
  *
  * Q: https://leetcode.com/problems/find-nearest-right-node-in-binary-tree/
- * A: https://leetcode.com/problems/find-nearest-right-node-in-binary-tree/discuss/875464/Javascript-Python3-C%2B%2B-BFS-solutions
+ * A: https://leetcode.com/problems/find-nearest-right-node-in-binary-tree/discuss/875464/Javascript-Python3-C%2B%2B-DFS-and-BFS
  */
 
 #include <iostream>
@@ -16,25 +16,48 @@ struct TreeNode {
     TreeNode(int val) : val{ val }, left{ nullptr }, right{ nullptr } {}
 };
 
-class Solution {
-public:
-    using Queue = queue<TreeNode*>;
-    TreeNode* findNearestRightNode(TreeNode* root, TreeNode* T) {
-        Queue q{{{ root }}};
-        while (q.size()) {
-            Queue next;
-            while (q.size()) {
-                auto node = q.front(); q.pop();
+namespace DFS {
+    class Solution {
+    public:
+        using fun = function<TreeNode*(TreeNode*, int)>;
+        TreeNode* findNearestRightNode(TreeNode* root, TreeNode* T, int found = -1) {
+            fun go = [&](auto node, auto depth) -> TreeNode* {
+                if (!node)
+                    return nullptr;
+                if (depth == found)
+                    return node;
                 if (node == T)
-                    return q.size() ? q.front() : nullptr;
-                if (node->left)  next.push(node->left);
-                if (node->right) next.push(node->right);
-            }
-            q.swap(next);
+                    found = depth;
+                auto L = go(node->left,  depth + 1),
+                     R = go(node->right, depth + 1);
+                return L ? L : R ? R : nullptr;
+            };
+            return go(root, 0);
         }
-        return nullptr;
-    }
-};
+    };
+}
+
+namespace BFS {
+    class Solution {
+    public:
+        using Queue = queue<TreeNode*>;
+        TreeNode* findNearestRightNode(TreeNode* root, TreeNode* T) {
+            Queue q{{{ root }}};
+            while (q.size()) {
+                Queue next;
+                while (q.size()) {
+                    auto node = q.front(); q.pop();
+                    if (node == T)
+                        return q.size() ? q.front() : nullptr;
+                    if (node->left)  next.push(node->left);
+                    if (node->right) next.push(node->right);
+                }
+                q.swap(next);
+            }
+            return nullptr;
+        }
+    };
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
