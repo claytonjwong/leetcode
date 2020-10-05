ff967d0e387753e1e644e7c99ac22d524cbbdc2c
Clayton Wong <claytonjwong@gmail.com>
2020-10-05 07:50:50 -0700

    532. K-diff Pairs in an Array

--- 532_k_diff_pairs/main.cpp
+++ 532_k_diff_pairs/main.cpp
@@ -2,12 +2,12 @@
  * 532. K-diff Pairs in an Array
  *
  * Q: https://leetcode.com/problems/k-diff-pairs-in-an-array/
- * A: https://leetcode.com/problems/k-diff-pairs-in-an-array/discuss/596830/Javascript-and-C%2B%2B-solutions
+ * A: https://leetcode.com/problems/k-diff-pairs-in-an-array/discuss/596830/Javascript-Python3-C%2B%2B-Concise-solutions
  */
 
 #include <iostream>
+#include <sstream>
 #include <vector>
-#include <unordered_map>
 #include <unordered_set>
 
 using namespace std;
@@ -15,16 +15,19 @@ using namespace std;
 class Solution {
 public:
     using VI = vector<int>;
-    using Map = unordered_map<int, int>;
-    using Set = unordered_set<int>;
-    int findPairs(VI& A, int K, Map m = {}, Set seen = {}) {
-        sort(A.begin(), A.end());
-        return count_if(A.begin(), A.end(), [&](auto x) {
-            if (!K)
-                return ++m[x] == 2;
-            else
-                return seen.insert(x).second && seen.find(x - K) != seen.end();
-        });
+    using Seen = unordered_set<int>;
+    using Set = unordered_set<string>;
+    int findPairs(VI& A, int K, Seen seen = {}, Set pairs = {}) {
+        auto pair = [&](auto a, auto b) {
+            stringstream stream; stream << min(a, b) << ',' << max(a, b);
+            pairs.insert(stream.str());
+        };
+        for (auto x: A) {
+            if (seen.find(x + K) != seen.end()) pair(x, x + K);
+            if (seen.find(x - K) != seen.end()) pair(x, x - K);
+            seen.insert(x);
+        }
+        return pairs.size();
     }
 };
 
