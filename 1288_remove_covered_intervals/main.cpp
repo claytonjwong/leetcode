8f725a600eebb8ef38bcf4a92ac9ec9d24a2a162
Clayton Wong <claytonjwong@gmail.com>
2020-10-05 07:09:30 -0700

    1288. Remove Covered Intervals

--- 1288_remove_covered_intervals/main.cpp
+++ 1288_remove_covered_intervals/main.cpp
@@ -2,70 +2,38 @@
  * 1288. Remove Covered Intervals
  *
  * Q: https://leetcode.com/problems/remove-covered-intervals/
- * A: https://leetcode.com/problems/remove-covered-intervals/discuss/457523/Javascript-and-C%2B%2B-solutions
+ * A: https://leetcode.com/problems/remove-covered-intervals/discuss/457523/Javascript-Python3-C%2B%2B-Sort-%2B-Linear-Scan
  */
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-/*
-
-since the interval length is <= 1000, then *maybe* a quadradic solution will *not* TLE,
-but i think i'll first try to code a O(nlogn) solution by sorting the intervals
-by their first value, then iterate through the sorted intervals and track the maximum ending value,
-if an interval's second value is <= the maximum ending value, then that interval is covered and thus removed.
-
-*/
-
-/*
 class Solution {
 public:
     using VI = vector<int>;
     using VVI = vector<VI>;
-    int removeCoveredIntervals(VVI& A, int max = 0, int cnt = 0) {
-        sort(A.begin(), A.end(), [](auto& a, auto& b) {
-            return a[0] < b[0];
+    int removeCoveredIntervals(VVI& A, int x = 0) {
+        sort(A.begin(), A.end(), [](auto& first, auto& second) {
+            auto [a, b] = tie(first[0], first[1]);
+            auto [c, d] = tie(second[0], second[1]);
+            return a == c ? d < b : a < c;
         });
-        for (auto i = 0; i < A.size(); ++i) {
-            auto [beg, end] = tie(A[i][0], A[i][1]);
-            if (end <= max)
-                continue; // covered
-            ++cnt;
-            max = end;
-        }
-        return cnt;
-    }
-};
-*/
-
-/*
-class Solution {
-public:
-    using VI = vector<int>;
-    using VVI = vector<VI>;
-    int removeCoveredIntervals(VVI& A, int max = 0, int cnt = 0) {
-        sort(A.begin(), A.end(), [](auto& a, auto& b) { return a[0] < b[0]; });
-        for (auto i = 0; i < A.size(); ++i)
-            if (A[i][1] > max)
-                max = A[i][1], ++cnt;
-        return cnt;
-    }
-};
-*/
-
-class Solution {
-public:
-    using VI = vector<int>;
-    using VVI = vector<VI>;
-    int removeCoveredIntervals(VVI& A, int max = 0, int cnt = 0) {
-        sort(A.begin(), A.end(), [](auto& a, auto& b) { return a[0] < b[0]; });
-        return count_if(A.begin(), A.end(), [&](auto& x) {
-            if (x[1] <= max)
-                return false;
-            max = x[1];
-            return true;
-        });
+        auto ok = [&](auto& first, auto& second) {
+            auto [a, b] = tie(first[0], first[1]);
+            auto [c, d] = tie(second[0], second[1]);
+            return a <= c && d <= b;
+        };
+        int N = A.size(),
+            i = 0,
+            j = 1;
+        while (j < N)
+            if (ok(A[i], A[j]))
+                ++x,
+                ++j;
+            else
+                i = j++;
+        return N - x;
     }
 };
 
