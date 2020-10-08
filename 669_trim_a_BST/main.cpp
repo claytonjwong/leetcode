7235ef8f1724aa361aed91f232132d4fbf2aff34
Clayton Wong <claytonjwong@gmail.com>
2020-10-08 08:47:52 -0700

    669. Trim a Binary Search Tree

--- 669_trim_a_BST/main.cpp
+++ 669_trim_a_BST/main.cpp
@@ -2,7 +2,7 @@
  * 669. Trim a Binary Search Tree
  *
  * Q: https://leetcode.com/problems/trim-a-binary-search-tree/
- * A: https://leetcode.com/problems/trim-a-binary-search-tree/discuss/599189/Javascript-and-C%2B%2B-solutions
+ * A: https://leetcode.com/problems/trim-a-binary-search-tree/discuss/599189/Javascript-Python3-C%2B%2B-Recursive
  */
 
 #include <iostream>
@@ -12,27 +12,56 @@ using namespace std;
 struct TreeNode {
     int val;
     TreeNode *left, *right;
-    TreeNode(int val) : val{val}, left{nullptr}, right{nullptr} {}
+    TreeNode(int val) : val{ val }, left{ nullptr }, right{ nullptr } {}
 };
 
-class Solution {
-public:
-    TreeNode* trimBST(TreeNode* root, int L, int R) {
-        if (!root)
+namespace Concise {
+    class Solution {
+    public:
+        using fun = function<void(TreeNode*)>;
+        TreeNode* trimBST(TreeNode* root, int lo, int hi) {
+            fun go = [&](auto root) {
+                while (root->left  && root->left->val < lo)  root->left  = root->left->right ? root->left->right : nullptr;
+                while (root->right && hi < root->right->val) root->right = root->right->left ? root->right->left : nullptr;
+                if (root->left)  go(root->left);
+                if (root->right) go(root->right);
+            };
+            go(root);
+            while (root && root->val < lo) root = root->right;
+            while (root && hi < root->val) root = root->left;
             return root;
-        go(root, L, R);
-        while (root && root->val < L) root = root->right;
-        while (root && root->val > R) root = root->left;
-        return root;
-    }
-private:
-    void go(TreeNode* root, int L, int R) {
-        while (root->left  && root->left->val  < L) root->left = root->left->right ? root->left->right : nullptr;
-        while (root->right && root->right->val > R) root->right = root->right->left ? root->right->left : nullptr;
-        if (root->left)  go(root->left,  L, R);
-        if (root->right) go(root->right, L, R);
-    }
-};
+        }
+    };
+}
+
+namespace Verbose {
+    class Solution {
+    public:
+        using fun = function<void(TreeNode*)>;
+        TreeNode* trimBST(TreeNode* root, int lo, int hi) {
+            fun go = [&](auto root) {
+                if (root->left && root->left->val < lo) {
+                    auto next = root->left->right;
+                    while (next && next->val < lo)
+                        next = next->right;
+                    root->left = next;
+                }
+                if (root->right && hi < root->right->val) {
+                    auto next = root->right->left;
+                    while (next && hi < next->val)
+                        next = next->left;
+                    root->right = next;
+                }
+                if (root->left)  go(root->left);
+                if (root->right) go(root->right);
+            };
+            go(root);
+            while (root && root->val < lo) root = root->right;
+            while (root && hi < root->val) root = root->left;
+            return root;
+        }
+    };
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
