f78c91965130a931ff208be23dc370a371492930
Clayton Wong <claytonjwong@gmail.com>
2020-10-08 07:12:25 -0700

    704. Binary Search

--- 704_binary_search/main.cpp
+++ 704_binary_search/main.cpp
@@ -2,7 +2,7 @@
  * 704. Binary Search
  *
  * Q: https://leetcode.com/problems/binary-search/
- * A: https://leetcode.com/problems/binary-search/discuss/600517/Javascript-and-C%2B%2B-solutions
+ * A: https://leetcode.com/problems/binary-search/discuss/600517/Javascript-Python3-C%2B%2B-Lower-Bound
  */
 
 #include <iostream>
@@ -10,23 +10,36 @@
 
 using namespace std;
 
-class Solution {
-public:
-    using VI = vector<int>;
-    int search(VI& A, int T) {
-        int N = A.size(),
-            i = 0,
-            j = N - 1;
-        while (i < j) {
-            auto k = (i + j) / 2;
-            if (A[k] < T)
-                i = k + 1;
-            else
-                j = k;
+namespace Concise {
+    class Solution {
+    public:
+        using VI = vector<int>;
+        int search(VI& A, int T) {
+            auto it = lower_bound(A.begin(), A.end(), T);
+            return it != A.end() && *it == T ? distance(A.begin(), it) : -1;
         }
-        return A[i] == T ? i : -1;
-    }
-};
+    };
+}
+
+namespace Verbose {
+    class Solution {
+    public:
+        using VI = vector<int>;
+        int search(VI& A, int T) {
+            int N = A.size(),
+                    i = 0,
+                    j = N - 1;
+            while (i < j) {
+                auto k = (i + j) / 2;
+                if (A[k] < T)
+                    i = k + 1;
+                else
+                    j = k;
+            }
+            return A[i] == T ? i : -1;
+        }
+    };
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
