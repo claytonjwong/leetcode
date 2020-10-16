e692100d9974aba31a88d89d04ef7ce8e7ff40a3
Clayton Wong <claytonjwong@gmail.com>
2020-10-16 10:41:32 -0700

    74. Search a 2D Matrix

--- 74_search_2D_matrix/main.cpp
+++ 74_search_2D_matrix/main.cpp
@@ -2,7 +2,7 @@
  * 74. Search a 2D Matrix
  *
  * Q: https://leetcode.com/problems/search-a-2d-matrix/
- * A: https://leetcode.com/problems/search-a-2d-matrix/discuss/605294/Javascript-and-C%2B%2B-solutions
+ * A: https://leetcode.com/problems/search-a-2d-matrix/discuss/605294/Kt-Js-Py3-Cpp-Upper-Bound-(ie.-Binary-Search)
  */
 
 #include <iostream>
@@ -14,25 +14,12 @@ class Solution {
 public:
     using VI = vector<int>;
     using VVI = vector<VI>;
-    bool searchMatrix(VVI& A, int T) {
-        int M = A.size(),
-            N = M ? A[0].size() : 0;
-        if (!M || !N)
+    bool searchMatrix(VVI& A, int T, VI first = {}) {
+        if (A.empty() || A[0].empty())
             return false;
-        auto find = [=](VI& A, int i, int j) {
-            while (i < j) {
-                auto k = (i + j) / 2;
-                if (T > A[k])
-                    i = k + 1;
-                else
-                    j = k;
-            }
-            return i;
-        };
-        VI rows; transform(A.begin(), A.end(), back_inserter(rows), [](auto& row) { return row.back(); });
-        auto i = find(rows, 0, M - 1);
-        auto j = find(A[i], 0, N - 1);
-        return A[i][j] == T;
+        transform(A.begin(), A.end(), back_inserter(first), [](auto& row) { return row[0]; });
+        auto row = int(distance(first.begin(), upper_bound(first.begin(), first.end(), T))) - 1;
+        return 0 <= row ? binary_search(A[row].begin(), A[row].end(), T) : false;
     }
 };
 
