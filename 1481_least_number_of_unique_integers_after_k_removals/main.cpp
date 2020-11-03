5b4ecb63a5bf892451ba133af72e33c7f0f062fe
Clayton Wong <claytonjwong@gmail.com>
2020-11-03 08:16:37 -0700

    1481. Least Number of Unique Integers after K Removals

--- 1481_least_number_of_unique_integers_after_k_removals/main.cpp
+++ 1481_least_number_of_unique_integers_after_k_removals/main.cpp
@@ -2,7 +2,7 @@
  * 1481. Least Number of Unique Integers after K Removals
  *
  * Q: https://leetcode.com/problems/least-number-of-unique-integers-after-k-removals/
- * A: https://leetcode.com/problems/least-number-of-unique-integers-after-k-removals/discuss/686410/Javascript-and-C%2B%2B-solutions
+ * A: https://leetcode.com/problems/least-number-of-unique-integers-after-k-removals/discuss/686410/Kt-Js-Py3-Cpp-Greedy-Drop-K-Least-Frequent-via-Map
  */
 
 #include <iostream>
@@ -16,16 +16,11 @@ class Solution {
 public:
     using VI = vector<int>;
     using Map = unordered_map<int, int>;
-    using Pairs = vector<pair<int, int>>;
     using Set = unordered_set<int>;
-    int findLeastNumOfUniqueInts(VI& A, int K, Map m = {}, VI all = {}) {
-        for (auto x: A)
-            ++m[x];
-        Pairs P{ m.begin(), m.end() };
-        sort(P.begin(), P.end(), [](auto& a, auto& b) { return a.second < b.second; }); // sort all by ascending frequency
-        for (auto [x, n]: P) // x occurs n times
-            fill_n(back_inserter(all), n, x);
-        return Set{ all.begin() + K, all.end() }.size(); // greedily drop first K least freq nums and return size of remaining set
+    int findLeastNumOfUniqueInts(VI& A, int K, Map m = {}) {
+        for (auto x: A) ++m[x];
+        sort(A.begin(), A.end(), [&](auto a, auto b) { return m[a] == m[b] ? a < b : m[a] < m[b]; });
+        return Set{ A.begin() + K, A.end() }.size();
     }
 };
 
