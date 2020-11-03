3ed766a5acef886edcba55c21458d8211a3b1b90
Clayton Wong <claytonjwong@gmail.com>
2020-11-03 06:27:11 -0700

    1446. Consecutive Characters

--- 1446_consecutive_chars/main.cpp
+++ 1446_consecutive_chars/main.cpp
@@ -2,24 +2,40 @@
  * 1446. Consecutive Characters
  *
  * Q: https://leetcode.com/problems/consecutive-characters/
- * A: https://leetcode.com/problems/consecutive-characters/discuss/639815/Javascript-and-C%2B%2B-solutions
+ * A: https://leetcode.com/problems/consecutive-characters/discuss/639815/Kt-Js-Py3-Cpp-Best-Same
  */
 
 #include <iostream>
 
 using namespace std;
 
-class Solution {
-public:
-    int maxPower(string s, int max = 1) {
-        for (auto i{ 1 }, same{ 1 }; i < s.size(); ++i)
-            if (s[i - 1] == s[i])
-                max = std::max(max, ++same);
-            else
-                same = 1;
-        return max;
-    }
-};
+namespace LastC {
+    class Solution {
+    public:
+        int maxPower(string s, char last = '0', int same = 1, int best = 1) {
+            for (auto c: s)
+                if (last == c)
+                    best = max(best, ++same);
+                else
+                    last = c, same = 1;
+            return best;
+        }
+    };
+}
+
+namespace Indexed {
+    class Solution {
+    public:
+        int maxPower(string s, int same = 1, int best = 1) {
+            for (auto i{ 1 }; i < s.size(); ++i)
+                if (s[i - 1] == s[i])
+                    best = max(best, ++same);
+                else
+                    same = 1;
+            return best;
+        }
+    };
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
