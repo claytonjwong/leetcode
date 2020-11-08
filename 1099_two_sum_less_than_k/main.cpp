8a5e2d547af616d0c84de00ef77546bbaf7ac172
Clayton Wong <claytonjwong@gmail.com>
2020-11-08 08:39:44 -0700

    1099. Two Sum Less Than K

--- 1099_two_sum_less_than_k/main.cpp
+++ 1099_two_sum_less_than_k/main.cpp
@@ -2,7 +2,7 @@
  * 1099. Two Sum Less Than K
  *
  * Q: https://leetcode.com/problems/two-sum-less-than-k/
- * A: https://leetcode.com/problems/two-sum-less-than-k/discuss/603797/Javascript-and-C%2B%2B-solutions
+ * A: https://leetcode.com/problems/two-sum-less-than-k/discuss/603797/Kt-Js-Py3-Cpp-Greedy-%2B-Brute-Force-solutions
  */
 
 #include <iostream>
@@ -11,21 +11,60 @@
 
 using namespace std;
 
-class Solution {
-public:
-    using VI = vector<int>;
-    using Set = set<int>;
-    int twoSumLessThanK(VI& A, int K, Set seen = {}, int max = -1) {
-        for (auto x: A) {
-            auto T = K - x;
-            auto it = seen.lower_bound(T);
-            if (it != seen.begin())
-                max = std::max(max, x + *--it);
-            seen.insert(x);
+namespace Greedy {
+    class Solution {
+    public:
+        using VI = vector<int>;
+        int twoSumLessThanK(VI& A, int K, int best = -1) {
+            sort(A.begin(), A.end());
+            int N = A.size(),
+                i = 0,
+                j = N - 1;
+            while (i < j) {
+                if (A[i] + A[j] < K) {
+                    best = max(best, A[i] + A[j]);
+                    ++i;
+                } else {
+                    --j;
+                }
+            }
+            return best;
         }
-        return max;
-    }
-};
+    };
+}
+
+namespace BruteForce {
+    class Solution {
+    public:
+        using VI = vector<int>;
+        int twoSumLessThanK(VI& A, int K, int best = -1) {
+            int N = A.size();
+            for (auto i{ 0 }; i < N; ++i)
+                for (auto j{ i + 1 }; j < N; ++j)
+                    if (A[i] + A[j] < K)
+                        best = max(best, A[i] + A[j]);
+            return best;
+        }
+    };
+}
+
+namespace LowerBound {
+    class Solution {
+    public:
+        using VI = vector<int>;
+        using Set = set<int>;
+        int twoSumLessThanK(VI &A, int K, Set seen = {}, int max = -1) {
+            for (auto x: A) {
+                auto T = K - x;
+                auto it = seen.lower_bound(T);
+                if (it != seen.begin())
+                    max = std::max(max, x + *--it);
+                seen.insert(x);
+            }
+            return max;
+        }
+    };
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
