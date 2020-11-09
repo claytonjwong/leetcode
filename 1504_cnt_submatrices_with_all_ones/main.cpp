385af8dc7f786a4c0adfec43e1640d1f0b38a60a
Clayton Wong <claytonjwong@gmail.com>
2020-11-09 13:40:09 -0700

    1504. Count Submatrices With All Ones

--- 1504_cnt_submatrices_with_all_ones/main.cpp
+++ 1504_cnt_submatrices_with_all_ones/main.cpp
@@ -2,7 +2,7 @@
  * 1504. Count Submatrices With All Ones
  *
  * Q: https://leetcode.com/problems/count-submatrices-with-all-ones/
- * A: https://leetcode.com/problems/count-submatrices-with-all-ones/discuss/762330/Javascript-Python3-C%2B%2B-brute-force
+ * A: https://leetcode.com/problems/count-submatrices-with-all-ones/discuss/762330/Kt-Js-Py3-Cpp-Brute-Force
  */
 
 #include <iostream>
@@ -11,28 +11,29 @@
 using namespace std;
 
 class Solution {
+public:
     using VI = vector<int>;
     using VVI = vector<VI>;
-    using Dirs = vector<tuple<int, int>>;
-    VVI A;
-    int go(int row, int col, int M, int N, int cnt = 0) {
-        for (auto i{ row }; i < M; ++i)
-            for (auto j{ col }; j < N; ++j)
-                if (A[i][j])
-                    ++cnt;
-                else
-                    N = j;
-        return cnt;
-    }
-public:
-    int numSubmat(VVI& A_, int ans = 0) {
-        A = A_;
-        int M = A.size(),
-                N = A[0].size();
+    using fun = function<int(int, int)>;
+    int numSubmat(VVI& A, int total = 0) {
+        fun go = [&](auto row, auto col) {
+            auto cnt{ 0 };
+            auto M = A.size(),
+                 N = A[0].size();
+            for (auto i{ row }; i < M; ++i)
+                for (auto j{ col }; j < N; ++j)
+                    if (A[i][j])
+                        ++cnt;
+                    else
+                        N = j;
+            return cnt;
+        };
+        auto M = A.size(),
+             N = A[0].size();
         for (auto i{ 0 }; i < M; ++i)
             for (auto j{ 0 }; j < N; ++j)
-                ans += go(i, j, M, N); // 🎯 count all submatrices starting with i,j as top-left corner
-        return ans;
+                total += go(i, j); // 🎯 count of all submatrices starting with i,j as top-left corner
+        return total;
     }
 };
 
