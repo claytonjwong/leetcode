a720c94a42f7282977ef7c719a352872ac220cef
Clayton Wong <claytonjwong@gmail.com>
2020-12-01 06:31:48 -0700

    243. Shortest Word Distance

--- 243_shortest_word_distance/main.cpp
+++ 243_shortest_word_distance/main.cpp
@@ -1,8 +1,8 @@
 /*
  * 243. Shortest Word Distance
  *
- * Q: https://leetcode.com/problems/shortest-word-distance-iii/
- * A: https://leetcode.com/problems/shortest-word-distance/discuss/670999/Javascript-and-C%2B%2B-solutions
+ * Q: https://leetcode.com/problems/shortest-word-distance/
+ * A: https://leetcode.com/problems/shortest-word-distance/discuss/670999/Kt-Js-Py3-Cpp-Last-Seen-K-th-Index
  */
 
 #include <iostream>
@@ -13,14 +13,14 @@ using namespace std;
 class Solution {
 public:
     using VS = vector<string>;
-    int shortestDistance(VS& A, string s, string t, int min = int(1e9 + 7)) {
-        for (auto i{ -1 }, j{ -1 }, k{ 0 }; k < A.size(); ++k) {
+    int shortestDistance(VS& A, string s, string t, int i = -1, int j = -1, int best = 1e9 + 7) {
+        for (auto k{ 0 }; k < A.size(); ++k) {
             if (A[k] == s) i = k;
             if (A[k] == t) j = k;
             if (-1 < i && -1 < j)
-                min = std::min(min, abs(i - j));
+                best = min(best, abs(i - j));
         }
-        return min;
+        return best;
     }
 };
 
