67be5b53b4ad39fccfe31c6477e0d92adafc9a10
Clayton Wong <claytonjwong@gmail.com>
2021-01-05 07:37:59 -0700

    82. Remove Duplicates from Sorted List II

--- 82_remove_duplicates/main.cpp
+++ 82_remove_duplicates/main.cpp
@@ -2,7 +2,7 @@
  * 82. Remove Duplicates from Sorted List II
  *
  * Q: https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
- * A: https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/discuss/590900/Javascript-and-C%2B%2B-solutions
+ * A: https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/discuss/590900/Kt-Js-Py3-Cpp-Single-Pass
  */
 
 #include <iostream>
@@ -12,14 +12,16 @@ using namespace std;
 struct ListNode {
     int val;
     ListNode* next;
-    ListNode(int val) : val{val}, next{nullptr} {}
+    ListNode(int val, ListNode* next) : val{ val }, next{ next } {}
 };
 
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        ListNode *sentinel = new ListNode(-1), *ans = sentinel;
-        ListNode *pre = head, *cur = head;
+        auto sentinel = new ListNode(-123, head),
+             ans = sentinel,
+             pre = head,
+             cur = head;
         while (cur) {
             while (cur->next && cur->val == cur->next->val)
                 cur = cur->next;
