52d034fc17834f9f1b8c5201a98a6b815fdaa13f
Clayton Wong <claytonjwong@gmail.com>
2021-01-06 08:28:27 -0700

    1539. Kth Missing Positive Number

--- 1539_kth_missing_positive_number/main.cpp
+++ 1539_kth_missing_positive_number/main.cpp
@@ -2,7 +2,7 @@
  * 1539. Kth Missing Positive Number
  *
  * Q: https://leetcode.com/problems/kth-missing-positive-number/
- * A: https://leetcode.com/problems/kth-missing-positive-number/discuss/780016/Javascript-Python3-C%2B%2B-solutions
+ * A: https://leetcode.com/problems/kth-missing-positive-number/discuss/780016/Kt-Js-Py3-Cpp-Seen-Set-%2B-Concise
  */
 
 #include <iostream>
@@ -11,17 +11,35 @@
 
 using namespace std;
 
-class Solution {
-public:
-    using VI = vector<int>;
-    using Set = unordered_set<int>;
-    int findKthPositive(VI& A, int K) {
-        Set seen{ A.begin(), A.end() };
-        for (auto i{ 1 };; ++i)
-            if (seen.find(i) == seen.end() && !--K)
-                return i;
-    }
-};
+namespace Seen {
+    class Solution {
+    public:
+        using VI = vector<int>;
+        using Set = unordered_set<int>;
+        int findKthPositive(VI& A, int K) {
+            Set seen{ A.begin(), A.end() };
+            for (auto i{ 1 };; ++i)
+                if (seen.find(i) == seen.end() && !--K)
+                    return i;
+        }
+    };
+}
+namespace Concise {
+    class Solution {
+    public:
+        using VI = vector<int>;
+        int findKthPositive(VI& A, int k) {
+            auto i = 0,
+                 x = 0;
+            while (k && i < A.size())
+                if (++x == A[i])
+                    ++i;
+                else
+                    --k;
+            return x + k;
+        }
+    };
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
