635ecc08eee1afb08c0dc770760710949985c573
Clayton Wong <claytonjwong@gmail.com>
2021-02-19 10:01:39 -0700

    338. Counting Bits

--- 338_counting_bits/main.cpp
+++ 338_counting_bits/main.cpp
@@ -2,7 +2,7 @@
  * 338. Counting Bits
  *
  * Q: https://leetcode.com/problems/counting-bits/
- * A: https://leetcode.com/problems/counting-bits/discuss/657068/Javascript-and-C%2B%2B-solutions
+ * A: https://leetcode.com/problems/counting-bits/discuss/657068/Kt-Js-Py3-Cpp-Dynamic-Programming
  */
 
 #include <iostream>
@@ -10,51 +10,15 @@
 
 using namespace std;
 
-namespace BruteForce {
-    class Solution {
-    public:
-        using VI = vector<int>;
-        VI countBits(int num, VI ans = {}) {
-            for (auto i{ 0 }; i <= num; ++i) {
-                auto bits{ 0 };
-                for (auto j{ 0 }; j < 32; ++j)
-                    if (i & (1 << j))
-                        ++bits;
-                ans.push_back(bits);
-            }
-            return ans;
-        }
-    };
-}
-
-namespace DP_Verbose {
-    class Solution {
-    public:
-        using VI = vector<int>;
-        VI countBits(int num, int pow = 0, VI ans = { 0 }) {
-            for (auto i{ 1 }; i <= num; ++i)
-                if (!(i & (i - 1)))
-                    ans.push_back(1), pow = i; // max pow2
-                else
-                    ans.push_back(1 + ans[i - pow]); // +1 for max pow2 + the prev ans w/o max pow2
-            return ans;
-        }
-    };
-}
-
-namespace DP_concise {
-    class Solution {
-    public:
-        using VI = vector<int>;
-        VI countBits(int num, int pow = 0, VI ans = { 0 }) {
-            for (auto i{ 1 }; i <= num; ++i) {
-                if (!(i & (i - 1))) pow = i;     // max pow2
-                ans.push_back(1 + ans[i - pow]); // +1 for max pow2 + prev ans w/o max pow2
-            }
-            return ans;
-        }
-    };
-}
+class Solution {
+public:
+    using VI = vector<int>;
+    VI countBits(int N, VI ans = { 0 }) {
+        for (auto i{ 1 }; i <= N; ++i)
+            ans.push_back(ans[i >> 1] + (i & 1));
+        return ans;
+    }
+};
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
